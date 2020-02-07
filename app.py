5db4a38b441d5cff6b020c82179f00b168385ac2
mjmel <matthewmelissa@gmail.com>
2020-02-07 00:18:29 -0500

    added deleterious functionality

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 import time 
 import altair as alt
 import scipy.special
+from sidebar import run_sidebar
+
+
 #%%
 
 
@@ -18,75 +21,15 @@ import scipy.special
 ###%%% bin first then display
 st.title('traveling fitness waves')
 st.text('Matthew Melissa, PhD Candidate at Harvard University')
+st.markdown('modeling the interplay of **mutation**, **selection** and **random genetic drift** in shaping the course of evolution') 
 
+N,Ub,Ud,sb,sd,draw_b,draw_d,num_gen = run_sidebar()
 
-st.markdown('evolution involves a complex interplay of **mutation**, **selection** and **genetic drift**') 
-
-N = 10**st.sidebar.slider('choose a population size',min_value=2.0,max_value=4.0,step=0.01,value=3.2,format='10E%f')
-U = 10**st.sidebar.slider('choose a mutation rate',min_value=-5.0,max_value=-2.0,step=0.01,value=-3.0,format='10E%f')
-
-
-dfe_option = st.sidebar.selectbox('choose a distribution of fitness effects of mutations',pd.Series(['single effect','exponential','gamma']))
-
-if dfe_option == 'gamma':
-
-	gamma_shape_opt = st.sidebar.slider('choose a gamma shape parameter',min_value=1.0,max_value=32.0,step=1.0,value=1.0)
-
-
-
-s = 10**st.sidebar.slider('choose an average effect size',min_value=-4.0,max_value=-1.0,step=0.01,value=-2.0,format='10E%f')
-
-num_gen = st.sidebar.slider('how many generations?',min_value=20,max_value=10000,step=10,value=1000)
-
-
-svals = np.linspace(0,4*s,100)
-
-if dfe_option == 'exponential':
-	dfe_source = pd.DataFrame({
-	  'fitness effect': svals,
-	  'probability': 1/s*np.exp(-svals/s)
-	})
-	def dfe():
-		return np.random.exponential(s)
-
-
-	c = alt.Chart(dfe_source,width=300).mark_area().encode(
-    x='fitness effect',
-    y='probability'
-
-	)
-	st.sidebar.altair_chart(c)
-
-elif dfe_option == 'gamma':
-	beta = gamma_shape/effect_size
-	dfe_source = pd.DataFrame({
-	  'fitness effect': svals,
-	  'probability': beta**gamma_shape/scipy.special.gamma(gamma_shape)*svals**(gamma_shape-1)*np.exp(-beta*svals)
-	})
-
-	c = alt.Chart(dfe_source,width=300).mark_area().encode(
-    x='fitness effect',
-    y='probability'
-
-	)
-	st.sidebar.altair_chart(c)
-
-
-elif dfe_option == 'single effect':
-	dfe_source = pd.DataFrame({
-	  'fitness effect': [s,s],
-	  'probability': [0,1]
-	})
-	def dfe():
-		return s
-#st.sidebar.area_chart(dfe_source).encode(x='fitness effect',y='probability')
-
-@st.cache(hash_funcs={np.ufunc:id},suppress_st_warning=True)
-def runsim(N,U,dfe,num_gen,seed):
+def runsim(N,Ub,Ud,draw_b,draw_d,num_gen,seed):
 	latest_iteration = st.empty()
-	lineages = [(1,)]
+	#lineages = [(1,)]
 	extant_lineages = np.array([0]).astype(int)
-	sizes = np.zeros((int(2*N*U*num_gen),num_gen))
+	sizes = np.zeros((int(2*N*(Ub+Ud)*num_gen),num_gen))
 	sizes[0,0] = N
 	fits =  np.array([1.0])
 	children = [[]]
@@ -100,17 +43,30 @@ def runsim(N,U,dfe,num_gen,seed):
 	    sizes[extant_lineages,t+1] = np.random.poisson(sizes[extant_lineages,t]*np.exp(np.array(fits[extant_lineages])-mean_fit)*np.exp(1-1/float(N)*np.sum(sizes[extant_lineages,t])))
 	    
 	    for j in extant_lineages:
-	        new_lineages = np.random.binomial(sizes[j,t+1],U)
+	        new_lineages = np.random.binomial(sizes[j,t+1],Ub)
 	        for k in range(new_lineages):
-	            lineages.append(lineages[j] + (np.random.randint(10**10),))
-	            extant_lineages = np.append(extant_lineages,len(lineages))
+	            #lineages.append(lineages[j] + (np.random.randint(10**10),))
+	            fits = np.append( fits,np.exp(draw_b())*fits[j] )
+	            extant_lineages = np.append(extant_lineages,len(fits))
 	            children.append([])
-	            children[j] += [len(lineages)-1]
+	            children[j] += [len(fits)-1]
 	            parents.append(())
 	            parents[-1] +=(j,)
-	            fits = np.append( fits,np.exp(dfe())*fits[j] )
-	            sizes[len(lineages)-1,t+1] = 1.0
+	            sizes[len(fits)-1,t+1] = 1.0
 	            sizes[j,t+1] -= 1.0
+	    for j in extant_lineages:
+	    	new_lineages_d = np.random.binomial(sizes[j,t+1],Ud)
+	    	for k in range(new_lineages_d):
+	            #lineages.append(lineages[j] + (np.random.randint(10**10),))
+	            fits = np.append( fits,np.exp(-draw_d())*fits[j] )
+	            extant_lineages = np.append(extant_lineages,len(fits))
+	            children.append([])
+	            children[j] += [len(fits)-1]
+	            parents.append(())
+	            parents[-1] +=(j,)
+	            sizes[len(fits)-1,t+1] = 1.0
+	            sizes[j,t+1] -= 1.0
+	            
 	            
 	    extant_lineages = extant_lineages[sizes[extant_lineages,t+1]>0]
 
@@ -159,8 +115,7 @@ def add_trads_parents(children,parents,sizes):
 
 
 
-@st.cache(allow_output_mutation=True)
-def plot_trajectories(N,U,s,num_gen,children,sizes):
+def plot_trajectories(N,num_gen,children,parents,sizes):
 	chart_data = pd.DataFrame([])
 	ki = 1
 	#arrlist = [list(add_trads(children,sizes,k)) for k in big_lineages[1:]]
@@ -177,27 +132,27 @@ def plot_trajectories(N,U,s,num_gen,children,sizes):
 	#chart_data = chart_data[chart_data['lineage']!='0']
 
 
-	gen_range = pd.DataFrame({'generation':np.linspace(0,num_gen,20).astype(int)})
+	gen_range = pd.DataFrame({'generation':np.linspace(0,num_gen,100).astype(int)})
 	c = alt.Chart(chart_data,width = 400).mark_line().encode(
 		x='generation:Q',
 
-		y=alt.Y('number individuals', scale=alt.Scale(domain=[0.0,1.1*N], clamp=True), title='number individuals'),
+		y=alt.Y('number individuals', scale=alt.Scale(domain=[0.0,1.1*N], clamp=True), title='number of individuals'),
 		color=alt.Color('lineage',legend=None)
 	)
 
 	line = alt.Chart(chart_data).mark_line().encode(
 		x='generation',
 
-		y=alt.Y('number individuals', scale=alt.Scale(domain=[0.0,1.1*N], clamp=True), title='number individuals'),
+		y=alt.Y('number individuals', scale=alt.Scale(domain=[0.0,1.1*N], clamp=True), title='number of individuals'),
 		color=alt.Color('lineage',legend=None)
 	).interactive()
 
-	nearest = alt.selection(type='single', nearest=True, on='mouseover',fields=['generation'], empty='none')
+	nearest = alt.selection(type='single', nearest=True, on='mouseover',fields=['generation'], empty='none',init={'generation':0})
 
 
-	slider = alt.binding_range(min=0, max=int(num_gen)-1, step=10, name='generation:')
-	selector = alt.selection_single(name="generation", fields=['generation'],
-                                bind=slider, init={'generation': 50})
+	#slider = alt.binding_range(min=0, max=int(num_gen)-1, step=10, name='generation:')
+	#selector = alt.selection_single(name="generation", fields=['generation'],
+                                #bind=slider, init={'generation': 50})
 
 	# rules = alt.Chart(gen_range).mark_rule(color='gray').encode(
  #    	x='generation',
@@ -222,10 +177,16 @@ def plot_trajectories(N,U,s,num_gen,children,sizes):
 	df_melt = df_melt[df_melt.number>0].astype({'generation': 'int32'})
 
 
+	if Ub == 0:
+		step_size = sd
+	elif Ud == 0:
+		step_size = sb
+	else:
+		step_size = np.min(sb,sd)
 
 	bchart = alt.Chart(df_melt,width = 600).mark_bar().encode(
-    	x=alt.Y('fitness:Q', bin=alt.Bin(extent=[1, max(df['fitness'])], step=s)),
-    	y=alt.Y('number:Q', scale=alt.Scale(domain=(0, N))),
+    	x=alt.Y('fitness:Q', bin=alt.Bin(extent=[min(df['fitness']), max(df['fitness'])], step=step_size), title='fitness'),
+    	y=alt.Y('number:Q', scale=alt.Scale(domain=(0, N)), title='number of individuals'),
     	color=alt.Color('lineage',legend=None)).add_selection(nearest).transform_filter(nearest).interactive()
 
 
@@ -244,16 +205,28 @@ if st.sidebar.button('Run simulation'):
 	#params = pd.read_csv('runparams.csv')
 	#N,U,s,num_gen,seed = params['N'][0],params['U'][0],params['s'][0],params['num_gen'][0],params['seed'][0]
 
-	children,parents,sizes,fits = runsim(N,U,dfe,num_gen,seed)
+	children,parents,sizes,fits = runsim(N,Ub,Ud,draw_b,draw_d,num_gen,seed)
 	bar.progress(50)
-	layerChart = plot_trajectories(N,U,s,num_gen,children,sizes)
+
+	layerChart = plot_trajectories(N,num_gen,children,parents,sizes)
 	bar.progress(75)
+	
+	imagine_mutations = st.empty()
+
 	st.altair_chart(layerChart  )
+	imagine_measuring = st.empty()
+
+
 	bar.progress(100)
+	time.sleep(0.1)
 
+	imagine_mutations.markdown('Imagine sequencing the DNA of all the individuals in a population (say, of *E. coli* or influenza viruses). The colored lines below show how many individuals carry each mutation over time.')
 
+	imagine_measuring.markdown('One could also measure the reproductive ability, or fitness, of the population over time. Mouse over the upper plot to see how the population moves through "fitness space" as a *traveling wave*.')
 
+	st.markdown('During my PhD, I have used a combination of analytical theory and simulations to describe the rate at which a population adapts and the shape of its traveling fitness wave. I have analyzed how these and other quantities depend on the size of the population, its mutation rate, and the distribution of fitness effects of new mutations, with a focus on large microbial populations.')
 
+	st.markdown('Source code and more details are available at <https://github.com/mjmel/evo-vis>.')
 
 
 
---
+++ sidebar.py
@@ -0,0 +1,162 @@
+import numpy as np
+import streamlit as st
+import pandas as pd 
+import altair as alt 
+import scipy.special
+
+def run_sidebar():
+
+	N = 10**st.sidebar.slider('choose a population size',min_value=2.0,max_value=4.0,step=0.01,value=3.2,format='10E%f')
+
+	beneficial = st.sidebar.checkbox('beneficial mutations',value=True)
+	deleterious = st.sidebar.checkbox('deleterious mutations',value=False)
+
+	if beneficial:
+		Ub = 10**st.sidebar.slider('choose a beneficial mutation rate',min_value=-5.0,max_value=-2.0,step=0.01,value=-3.0,format='10E%f')
+	else:
+		Ub = 0
+	if deleterious:
+		Ud = 10**st.sidebar.slider('choose a deleterious mutation rate',min_value=-5.0,max_value=-2.0,step=0.01,value=-3.0,format='10E%f')
+	else:
+		Ud = 0
+
+	if beneficial:
+		sb = 10**st.sidebar.slider('choose an average beneficial effect size',min_value=-4.0,max_value=-1.0,step=0.01,value=-2.0,format='10E%f')
+	else:
+		sb = 0
+
+	if deleterious:
+		sd = 10**st.sidebar.slider('choose an average deleterious effect size',min_value=-4.0,max_value=-1.0,step=0.01,value=-2.0,format='10E%f')
+	else:
+		sd = 0
+
+	if beneficial:
+		dfeb_option = st.sidebar.selectbox('choose a distribution of fitness effects of beneficial mutations',pd.Series(['single effect','exponential','gamma']))
+
+		if dfeb_option == 'gamma':
+
+			gamma_shape_b = st.sidebar.slider('choose a (beneficial) gamma shape parameter',min_value=1.0,max_value=32.0,step=1.0,value=1.0)
+	else:
+		dfeb_option = 'none'
+
+	if deleterious:
+		dfed_option = st.sidebar.selectbox('choose a distribution of fitness effects of deleterious mutations',pd.Series(['single effect','exponential','gamma']))
+
+		if dfed_option == 'gamma':
+
+			gamma_shape_d = st.sidebar.slider('choose a (deleterious) gamma shape parameter',min_value=1.0,max_value=32.0,step=1.0,value=1.0)
+	else:
+		dfed_option = 'none'
+
+
+	num_gen = st.sidebar.slider('how many generations?',min_value=20,max_value=10000,step=10,value=1000)
+
+
+	sbvals = np.linspace(0,4*sb,100)
+	sdvals = -np.linspace(4*sd,0,100)
+
+
+	if beneficial:
+		if dfeb_option == 'exponential':
+			dfe_source = pd.DataFrame({
+			  'fitness effect': sbvals,
+			  'probability': Ub/(Ub+Ud)/sb*np.exp(-sbvals/sb)
+			})
+			dfe_b = lambda : np.random.exponential(sb)
+
+
+			cb = alt.Chart(dfe_source,width=300).mark_area().encode(
+		    x='fitness effect',
+		    y='probability'
+
+			)
+
+		elif dfeb_option == 'gamma':
+			beta_b = gamma_shape_b/sb
+			dfe_source = pd.DataFrame({
+			  'fitness effect': sbvals,
+			  'probability': Ub/(Ub+Ud)*beta_b**gamma_shape_b/scipy.special.gamma(gamma_shape_b)*sbvals**(gamma_shape_b-1)*np.exp(-beta_b*sbvals)
+			})
+
+			dfe_b = lambda : np.random.gamma(gamma_shape_b,1/beta_b)
+
+			cb = alt.Chart(dfe_source,width=300).mark_area().encode(
+		    x='fitness effect',
+		    y='probability'
+
+			)
+			
+
+
+		elif dfeb_option == 'single effect':
+			delta = 0.02
+			dfe_source = pd.DataFrame({
+			  'fitness effect': [sb*(1-delta),sb*(1+delta)],
+			  'probability': [Ub/(Ub+Ud)/2.0/delta/sb,Ub/(Ub+Ud)/2.0/delta/sb]
+			})
+			dfe_b = lambda : sb
+
+
+			cb = alt.Chart(dfe_source,width=300).mark_area().encode(
+		    	x=alt.X('fitness effect', scale=alt.Scale(domain=[-4*sd,4*sb])),
+		    	y='probability'
+			)
+	else:
+		dfe_b = lambda : 0
+
+
+	if deleterious:
+		if dfed_option == 'exponential':
+			dfed_source = pd.DataFrame({
+			  'fitness effect': sdvals,
+			  'probability': Ud/(Ub+Ud)/sd*np.exp(sdvals/sd)
+			})
+			dfe_d = lambda : np.random.exponential(sd)
+
+
+			cd = alt.Chart(dfed_source,width=300).mark_area(color='red').encode(
+		    	x='fitness effect',
+		    	y='probability'
+
+			)
+
+		elif dfed_option == 'gamma':
+			beta_d = gamma_shape_d/sd
+			dfed_source = pd.DataFrame({
+			  'fitness effect': sdvals,
+			  'probability': Ud/(Ub+Ud)*beta_d**gamma_shape_d/scipy.special.gamma(gamma_shape_d)*(-sdvals)**(gamma_shape_d-1)*np.exp(beta_d*sdvals)
+			})
+			dfe_d = lambda : np.random.gamma(gamma_shape_d,1/beta_d)
+
+			cd = alt.Chart(dfed_source,width=300).mark_area(color='red').encode(
+		    x='fitness effect',
+		    y='probability'
+
+			)
+			
+		elif dfed_option == 'single effect':
+			delta = 0.02
+			dfe_source = pd.DataFrame({
+			  'fitness effect': [-sd*(1-delta),-sd*(1+delta)],
+			  'probability': [Ud/(Ub+Ud)/2.0/delta/sd,Ud/(Ub+Ud)/2.0/delta/sd]
+			})
+			dfe_d = lambda : sd
+
+			cd = alt.Chart(dfe_source,width=300).mark_area(color='red').encode(
+		    	x=alt.X('fitness effect', scale=alt.Scale(domain=[-4*sd,4*sb])),
+		    	y='probability'
+			)
+	else:
+		dfe_d = lambda : 0
+
+
+	if beneficial and deleterious:
+		st.sidebar.altair_chart(cb+cd)
+	elif beneficial:
+		st.sidebar.altair_chart(cb)
+	elif deleterious:
+		st.sidebar.altair_chart(cd)
+
+	
+	return N,Ub,Ud,sb,sd,dfe_b,dfe_d,num_gen
+
